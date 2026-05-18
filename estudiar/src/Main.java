033a2458161f13f9a014341e481bfa303bb1dd45
Daniel Parra <danielitox767@gmail.com>
2026-05-17 20:14:14 -0400

    Update Main.java

--- estudiar/src/Main.java
+++ estudiar/src/Main.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("programa chachi");
         frame.setSize(400,500);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        frame.add(new JButton("BOTON1"));
+        frame.add(new JButton("BOTON2"));
+        frame.add(new JButton("BOTON2"));
+        frame.setVisible(true);
+
     }
 }
