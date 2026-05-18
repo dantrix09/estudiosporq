11b33885fb8f6e1a8f9b31bb1e0a6f7325ca8dde
Daniel Parra <danielitox767@gmail.com>
2026-05-17 20:03:42 -0400

    asd

---
+++ estudiar/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("programa chachi");
+        frame.setSize(400,500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
